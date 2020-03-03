fbd7870b9bbfc9fc0fe93091dba0f545959a545a
Jaime Leonardo Suncin Cruz <leosuncin@gmail.com>
2020-03-03 15:36:21 -0600

    feat: Try to open app with links

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ import { NextPage } from 'next'
 
 const IndexPage: NextPage = () => {
   return (
-    <h1>Spotify</h1>
+    <h1>Spotify <a href="spotify-remote-control://--" rel="noopener noreferrer">open app</a> or <a href="exp://exp.host/@suncin/spotify-remote-control">expo</a></h1>
   )
 }
 
